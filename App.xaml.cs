cc48ad125639bcdee2e35690f4589df853f7b88d
kasim555 <kasimkotean846@gmail.com>
2024-12-21 12:38:31 +0300

    ready

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _starting_monkey_to_human_path_
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
