776c8824139f49d298f0d3f256cfe665a016fd7c
kasim555 <kasimkotean846@gmail.com>
2024-12-21 12:56:40 +0300

    Added entry point

---
+++ 323/Kotean Kasim/wdad/Application.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _starting_monkey_to_human_path_._323.Kotean_Kasim.wdad
+{
+    internal class Application
+    {
+        public static void main(String[] args)
+        {
+            Console.WriteLine("I’m Касим Котеян, and I’m not a monkey");
+        }
+
+    }
+}
